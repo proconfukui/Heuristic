751bc46038c57bf12e78b2248b76aff469352372
Naruki7 <naru_7_7@icloud.com>
2025-09-26 23:53:12 +0900

    仮の状態です

--- create_answer_json.cpp
+++ create_answer_json.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
-int main(){
-  cout << "Hello world." << std::endl;
+#include "json.hpp"
+using json = nlohmann::ordered_json;
+
+struct ops {
+  int x;
+  int y;
+  int n;
+};
+
+// この関数でキーを追加した順番が保持されるようになります
+void to_json(json& j, const ops& o) {
+  j = json{
+      {"x", o.x},  // 1番目
+      {"y", o.y},  // 2番目
+      {"n", o.n}   // 3番目
+  };
+}
+
+int main() {
+  std::vector<ops> data = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+
+  json j;
+  j["ops"] = data;
+
+  // dump(4)で整形して出力
+  std::cout << j.dump(4) << std::endl;
+
   return 0;
 }
--- input_problem.cpp
+++ input_problem.cpp
@@ -1,7 +1,49 @@
+#include <fstream>
 #include <iostream>
+
+#include "json.hpp"
+
+using json = nlohmann::json;
 using namespace std;
 
-int main(){
-  cout << "Hello world." << std::endl;
+int main() {
+  // JSONファイル読み込み
+  ifstream ifs("input.json");
+  if (!ifs) {
+    cerr << "JSONファイルが開けませんでした。" << endl;
+    return 1;
+  }
+
+  json data;
+  ifs >> data;
+
+  // startsAtの出力
+  if (data.contains("startsAt")) {
+    cout << data["startsAt"] << endl;
+  } else {
+    cout << "startsAt: 未定義" << endl;
+  }
+
+  // sizeの出力
+  if (data.contains("problem") && data["problem"].contains("field") &&
+      data["problem"]["field"].contains("size")) {
+    cout << data["problem"]["field"]["size"] << endl;
+  } else {
+    cout << "size: 未定義" << endl;
+  }
+
+  // entitiesの出力
+  if (data.contains("problem") && data["problem"].contains("field") &&
+      data["problem"]["field"].contains("entities")) {
+    for (const auto& row : data["problem"]["field"]["entities"]) {
+      for (const auto& val : row) {
+        cout << val << " ";
+      }
+      cout << endl;
+    }
+  } else {
+    cout << "entities: 未定義" << endl;
+  }
+
   return 0;
 }
